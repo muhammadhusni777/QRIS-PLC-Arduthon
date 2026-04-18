203150b5f47c3c4bb20345ee522d2d3fca45acfd
Muhammad Husni <52745045+muhammadhusni777@users.noreply.github.com>
2026-04-18 18:01:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ import numpy as np
 
 # Initialize the API client with your Mayar API Key
 
-api = Mayar("YOUR API KEY")
+api = Mayar("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJiMTg5OWM0My01YjJkLTRlOGUtYmMyZS1jZDI2NGI3ZTE0ZDgiLCJhY2NvdW50SWQiOiI5NTIzNWFlYy1iNjJjLTQ0NjMtYjJlNC1kYzUwZWZkZWYyYWMiLCJjcmVhdGVkQXQiOiIxNzc2MzI3MDM2NTg5Iiwicm9sZSI6ImRldmVsb3BlciIsInN1YiI6Imh1c25pbXV0dGFxaW43NzdAZ21haWwuY29tIiwibmFtZSI6ImtlbGFzcm9ib3QiLCJsaW5rIjoiaHVzbmktbXV0dGFxaW4iLCJpc1NlbGZEb21haW4iOm51bGwsImlhdCI6MTc3NjMyNzAzNn0.Jbuaxfd0eDlgGwPHvDQc0HtIoPYqh1lbnj85MTVqzBb80mTZW3uWDRxI69NvTIG62fJ0egLKmTER9TXkz8M1Q4NajFvAHzE2NElk2qJSsM-QEcBhKvzrF7Zwupap3SfrSamqkt4qVv-dP19oo66cLKag32QmPNUab2_ZtdcQLefoldva6oTc1EQZRPvLA953zS3i6gtj42fJpDjWtm7-PIp0dXJTkWj51_Fdv_ENkr04oSC2cygnYHPOBUheYfaOYIpVlT45O6yH1jPxQt8gTR-RThsTUuywQu60fbggEhxc9f3H1MNx3Gch32K3-cnJBvGyuVmFr5kJPU207sEcmQ")
+
 
 time_now = datetime.now()
 id_order = ""
@@ -44,6 +45,7 @@ vending_balance = 0
 balance_prev = 0
 
 show_qr = 0
+boot = 0
 #----------------------------------------------------------------#
 
 class Worker(QThread):
@@ -60,10 +62,17 @@ class Worker(QThread):
             global balance_prev
             global success_pay
             global show_qr
+            global boot
             balance = api.balance()
+            
             
             if balance.status_code == 200:
                 vending_balance = balance.balance
+                
+                if (boot == 0):
+                    balance_prev = balance.balance
+                    boot = 1
+                    
                 #print(f"Balance: {vending_balance}")
             else:
                 print(f"Error: {balance.message}")
